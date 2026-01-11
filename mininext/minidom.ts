a76de975da88940aec135ab6616ef5aded5890f7
Spirobel <spirobel@protonmail.com>
2026-01-12 01:25:27 +0800

    we dont need a write queue. the recursion in render makes it such that the tree of components is built first before it is attached .

--- mininext/minidom.ts
+++ mininext/minidom.ts
@@ -29,50 +29,7 @@ function startRafLoop() {
       const resolved = evaluated.resolve(mini);
       root.cac = resolved.render(root.container, cac);
     }
-    flushDomUpdates();
     requestAnimationFrame(loop);
   }
   requestAnimationFrame(loop);
-}
-const writeQueue: Array<DomUpdateOptions> = [];
-
-function flushDomUpdates() {
-  for (const write of writeQueue) {
-    replace(write);
-  }
-  writeQueue.length = 0;
-}
-
-type DomUpdateOptions = {
-  // Text update: update this Text node's content.
-  textTarget?: Text;
-  text?: string;
-
-  // Structural replacement: replace `target` with `replacement`.
-  target?: Element | HTMLElement;
-  replacement?: Element;
-};
-export function replace(options: DomUpdateOptions): void {
-  const { textTarget, text, target, replacement } = options;
-  if (textTarget) {
-    if (!textTarget.isConnected) return;
-
-    if (typeof text === "string") {
-      if (textTarget.textContent === text) return;
-      textTarget.textContent = text;
-    }
-    return;
-  }
-
-  if (target && replacement) {
-    // if (!target.isConnected) return;
-
-    const parent = target.parentNode;
-    if (!parent) return;
-    parent.replaceChild(replacement, target);
-  }
-}
-// only cache hits trigger dom update schedules ( if the value changed )
-export function scheduleDomUpdate(options: DomUpdateOptions): void {
-  writeQueue.push(options);
 }
