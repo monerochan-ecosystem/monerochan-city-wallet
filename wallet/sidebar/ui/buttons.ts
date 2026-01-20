a801151ee4fe2da5eb5f861082a4f06b36d6125a
Spirobel <spirobel@protonmail.com>
2026-01-21 02:03:10 +0800

    no point in drilling mini. the components are purely declarative, state is handled in the lower component

--- wallet/sidebar/ui/buttons.ts
+++ wallet/sidebar/ui/buttons.ts
@@ -1,46 +1,58 @@
-import type { Mini, MiniHtmlString } from "../../../mininext/mininext";
+import { html, type MiniHtmlString } from "../../../mininext/mininext";
 
-export function tactileSwitch(buttonText: string | MiniHtmlString) {
-  return (mini: Mini) => {
-    return mini.html`<div class="tactile-switch">${buttonText}<style>
-    .tactile-switch{
-      border-radius:3% 10% 6% 16% / 18% 72% 21% 74%  ;
-      height: 30px;
-      min-width: 75px;
-      margin-right: 7px;
-      color: rgba(255, 255, 255, 0.3);
-      background-color: #666;
-      background-image:
-       linear-gradient(to bottom,
-         transparent 0px 2px,
-          rgba(0, 0, 0, 0.5) 2px 5px,
-         transparent 5px 7px,
-         rgba(0, 0, 0, 0.5) 7px 10px,
-         rgba(0, 0, 0, 0.14) 10px 16px);
-      outline: 2px solid rgba(102, 102, 102,0.5);
-      display: flex;
-      align-items: flex-end; 
-      justify-content: flex-end; 
-      padding: 0px 4px;
-      box-sizing: border-box;
-      font-size: 12px;
-      font-weight: bold;
-      box-shadow: inset 0 4px 12px rgba(0, 0, 0, 0.45), 0 15px 25px rgba(0, 0, 0, 0.4);
-      cursor: pointer;
-
-    }
-    .tactile-switch:hover {
-    color: rgba(255, 255, 255, 0.7);
-}
-  </style></div>`;
+export function tactileSwitch(id: string, buttonText: string | MiniHtmlString) {
+  return () => {
+    return html`<div id="${id}" class="tactile-switch">
+      ${buttonText}<style>
+        .tactile-switch {
+          border-radius: 3% 10% 6% 16% / 18% 72% 21% 74%;
+          height: 30px;
+          min-width: 75px;
+          margin-right: 7px;
+          color: rgba(255, 255, 255, 0.3);
+          background-color: #666;
+          background-image: linear-gradient(
+            to bottom,
+            transparent 0px 2px,
+            rgba(0, 0, 0, 0.5) 2px 5px,
+            transparent 5px 7px,
+            rgba(0, 0, 0, 0.5) 7px 10px,
+            rgba(0, 0, 0, 0.14) 10px 16px
+          );
+          outline: 2px solid rgba(102, 102, 102, 0.5);
+          display: flex;
+          align-items: flex-end;
+          justify-content: flex-end;
+          padding: 0px 4px;
+          box-sizing: border-box;
+          font-size: 12px;
+          font-weight: bold;
+          box-shadow:
+            inset 0 4px 12px rgba(0, 0, 0, 0.45),
+            0 15px 25px rgba(0, 0, 0, 0.4);
+          cursor: pointer;
+        }
+        .tactile-switch:hover {
+          color: rgba(255, 255, 255, 0.7);
+        }
+      </style>
+    </div>`;
   };
 }
-export function lowerTopMenu(mini: Mini) {
-  const historyButton = tactileSwitch("HISTORY");
-  const receiveButton = tactileSwitch("RECEIVE");
+export const buttonIds = {
+  send: "send",
+  receive: "receive",
+  history: "history",
+  wallets: "wallets",
+  connection: "connection",
+} as const;
+export function lowerTopMenu() {
+  const historyButton = tactileSwitch(buttonIds.history, "HISTORY");
+  const receiveButton = tactileSwitch(buttonIds.receive, "RECEIVE");
 
   const sendButton = tactileSwitch(
-    mini.html`<span>
+    buttonIds.send,
+    html`<span>
       <style>
         .red-dot {
           height: 4px;
@@ -55,34 +67,33 @@ export function lowerTopMenu(mini: Mini) {
       SEND <span class="red-dot"></span
     ></span>`,
   );
-  return mini.html`<div class="top-menu">
-            ${sendButton(mini)}
-            ${receiveButton(mini)}
-             <div></div>
-              ${historyButton(mini)}
-            <style>
-                .top-menu {
-                display: grid;
-                grid-template-columns: 80px 80px 1fr 80px; 
-                justify-content: start; 
-                gap: 8px; 
-                margin-left: 8px;
-                }
-          </style>
-          </div>  
-`;
+  return html`<div class="top-menu">
+    ${sendButton()} ${receiveButton()}
+    <div></div>
+    ${historyButton()}
+    <style>
+      .top-menu {
+        display: grid;
+        grid-template-columns: 80px 80px 1fr 80px;
+        justify-content: start;
+        gap: 8px;
+        margin-left: 8px;
+      }
+    </style>
+  </div> `;
 }
-export function lowerBottomMenu(mini: Mini) {
-  const walletsButton = tactileSwitch("WALLETS");
+export function lowerBottomMenu() {
+  const walletsButton = tactileSwitch(buttonIds.wallets, "WALLETS");
 
   const connectionButton = tactileSwitch(
-    mini.html`<span>
+    buttonIds.connection,
+    html`<span>
       <style>
         .green-dot {
           height: 4px;
           width: 4px;
           margin: 2px;
-          background-color: #4ADE80;
+          background-color: #4ade80;
           box-shadow: 0 1px 2px rgba(0, 0, 0, 0.8);
           border-radius: 50%;
           display: inline-block;
@@ -91,20 +102,19 @@ export function lowerBottomMenu(mini: Mini) {
       CONNECTION <span class="green-dot"></span
     ></span>`,
   );
-  return mini.html`<div class="bottom-menu">
-            ${connectionButton(mini)}
-             <div></div>
-              ${walletsButton(mini)}
-            <style>
-                .bottom-menu {
-                display: grid;
-                grid-template-columns: 140px 1fr 140px; 
-                justify-content: start; 
-                gap: 8px; 
-                margin-left: 8px;
-                margin-top: 50px;
-                }
-          </style>
-          </div>  
-`;
+  return html`<div class="bottom-menu">
+    ${connectionButton()}
+    <div></div>
+    ${walletsButton()}
+    <style>
+      .bottom-menu {
+        display: grid;
+        grid-template-columns: 140px 1fr 140px;
+        justify-content: start;
+        gap: 8px;
+        margin-left: 8px;
+        margin-top: 50px;
+      }
+    </style>
+  </div> `;
 }
