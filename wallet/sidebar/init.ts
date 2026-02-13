45b206f92125618cd37ca8478c6f9c246bf3fbff
Spirobel <spirobel@protonmail.com>
2026-02-14 02:45:18 +0800

    add sidebar init route

---
+++ wallet/sidebar/init.ts
@@ -0,0 +1,33 @@
+import {
+  openWallets,
+  SCAN_SETTINGS_STORE_NAME_DEFAULT,
+} from "@spirobel/monero-wallet-api";
+import {
+  receiveWalletChangedEvent,
+  type ExtensionMessage,
+} from "../../background/messagebus";
+import { router } from "./router";
+
+if (typeof chrome !== "undefined" && typeof browser === "undefined") {
+  globalThis.browser = chrome;
+}
+export async function init() {
+  const scan_settings_file = await Bun.file(SCAN_SETTINGS_STORE_NAME_DEFAULT);
+  if (scan_settings_file.size === 0) {
+    router.navigate("/onboarding");
+    return;
+  }
+  window.wallets = await openWallets({
+    no_worker: true,
+  });
+  //if (!wallets) throw new Error("Could not open wallets");
+  console.log("wallets", window.wallets);
+
+  if (browser.runtime) {
+    browser.runtime.onMessage.addListener((msg: ExtensionMessage, sender) => {
+      receiveWalletChangedEvent(msg, (payload) => {
+        window.wallets?.feed(payload);
+      });
+    });
+  }
+}
