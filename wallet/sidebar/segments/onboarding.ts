d3d100c2f122535acdf8660c5d6d152912d43bd7
Spirobel <spirobel@protonmail.com>
2026-02-14 16:21:33 +0800

    add seedphrase display + seedoffset passphrase display

--- wallet/sidebar/segments/onboarding.ts
+++ wallet/sidebar/segments/onboarding.ts
@@ -1,5 +1,7 @@
-import { html } from "../../../mininext/mininext";
+import { flatten, html } from "../../../mininext/mininext";
+import { generateSeedphrase } from "@spirobel/seedphrase";
 
+const seedphrase = generateSeedphrase().split(" ");
 export function onboarding() {
   return html`<div class="main">
     <style>
@@ -11,7 +13,16 @@ export function onboarding() {
         gap: 11px;
       }
     </style>
-    <div class="upper">
+    ${onboardingUpper()}
+  </div>`;
+}
+
+function onboardingUpper() {
+  const seedwords = seedphrase.map((word, i) => {
+    return html`<div class="seed-phrase" id="word${i + 1}">${word}</div>`;
+  });
+  return flatten(seedwords, (htmlstrings) => {
+    return html`<div class="upper">
       <style>
         .upper {
           height: 199px;
@@ -23,19 +34,65 @@ export function onboarding() {
             0 15px 25px rgba(0, 0, 0, 0.4);
           display: grid;
           grid-template-areas:
-            "labels labels labels"
-            "track track track"
-            "multi multi multi"
-            "divider divider divider"
-            "wallet wallet wallet";
-          grid-template-rows: 30px 16px 30px 4px 40px;
-          grid-template-columns: 30px 1fr 30px;
-          padding: 20px 30px;
+            "seedphrase seedphrase seedphrase seedphrase"
+            "seedphrase seedphrase seedphrase seedphrase"
+            "seedphrase seedphrase seedphrase seedphrase"
+            "divider divider divider divider"
+            "passphrase passphrase passphrase passphrase";
+          grid-template-columns: repeat(4, 1fr);
+          grid-template-rows: repeat(5, 20px);
           box-sizing: border-box;
-          gap: 10px 0;
+          gap: 14px;
+          padding: 17px;
+        }
+        .seed-phrase {
+          font-size: 14px;
+          font-weight: bold;
+          color: #bbb;
+          text-shadow: 0 1px 2px rgba(0, 0, 0, 0.8);
+        }
+        .divider {
+          margin-top: 10px;
+          grid-area: divider;
+          height: 4px;
+          background: #666;
+          border-radius: 12px;
+          box-shadow: 0 0 12px rgba(0, 0, 0, 0.6);
+        }
+        .passphrase {
+          grid-area: passphrase;
+          font-size: 14px;
+          font-weight: bold;
+          color: #bbb;
+          text-shadow: 0 1px 2px rgba(0, 0, 0, 0.8);
+        }
+        .divider::before {
+          content: "seedphrase";
+          margin-top: -14px;
+          float: right;
+          font-size: 10px;
+          color: #888;
+          font-weight: normal;
           user-select: none;
+          pointer-events: none;
+          white-space: nowrap;
+        }
+
+        .divider::after {
+          content: "seedoffset passphrase";
+          margin-top: 7px;
+          float: right;
+          font-size: 10px;
+          color: #888;
+          font-weight: normal;
+          user-select: none;
+          pointer-events: none;
+          white-space: nowrap;
         }
       </style>
-    </div>
-  </div>`;
+      ${htmlstrings}
+      <div class="divider"></div>
+      <div class="passphrase">this is my secret password</div>
+    </div>`;
+  });
 }
