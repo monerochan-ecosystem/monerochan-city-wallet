01b0a044183252caf9151743dbd522dcda11e0a1
Spirobel <spirobel@protonmail.com>
2026-02-22 19:32:14 +0800

    add sidebar entrypoint

---
+++ wallet/sidebar/sidebar.ts
@@ -0,0 +1,11 @@
+import { html, renderRoot } from "../../mininext/mininext";
+import { router } from "./router";
+import { init } from "./init";
+
+const container = document.getElementById("container");
+if (!container) throw new Error("Could not find container element");
+renderRoot({
+  component: () => html`<div style="height: 100%">${router.component}</div>`,
+  container,
+});
+await init();
