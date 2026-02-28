8b0d5083ca8752990453e8fe4ab9e0d49ceab882
Spirobel <spirobel@protonmail.com>
2026-03-01 02:40:06 +0800

    add spendable amounts to wallets list

--- wallet/sidebar/segments/wallets.ts
+++ wallet/sidebar/segments/wallets.ts
@@ -1,3 +1,7 @@
+import {
+  convertBigIntAmount,
+  truncateDecimalString,
+} from "@spirobel/monero-wallet-api";
 import { flatten, html, type MiniHtmlString } from "../../../mininext/mininext";
 import { router } from "../router";
 import { rightLower, tactileContentPlate } from "../ui/content";
@@ -5,10 +9,18 @@ import { allWallets } from "./walletRoute";
 
 export function walletsPlate() {
   const walletsList: () => MiniHtmlString = () => {
-    const wl = allWallets().map(
-      (wallet) =>
-        html`<a class="wallet" href=${router.link(wallet.wallet_route || "")}>${wallet.wallet_route || '""'}</div>`,
-    ) || [html`<div class="wallet">no wallets</div>`];
+    const wl = allWallets().map((wallet) => {
+      const colorclass =
+        wallet.amount || 0n > 0 ? "amount-positive" : "amount-zero";
+      const amount = convertBigIntAmount(wallet.amount || 0n);
+      const amountTrun = truncateDecimalString(amount, 3);
+      return html`<div class="wallet-route">
+        <a class="wallet" href=${router.link(wallet.wallet_route || "")}>
+          ${wallet.wallet_route || '""'}
+        </a>
+        <div class="amount ${colorclass}">${amountTrun}</div>
+      </div>`;
+    }) || [html`<div class="wallet">no wallets</div>`];
     return flatten(wl);
   };
 
@@ -23,6 +35,18 @@ export function walletsPlate() {
           font-family: sans-serif;
           font-size: 16px;
           font-weight: 700;
+        }
+        .wallet-route {
+          display: grid;
+          grid-template-columns: 200px 1fr;
+          gap: 28px;
+          place-items: baseline;
+        }
+        .amount-positive {
+          color: #ff4444;
+        }
+        .amount-zero {
+          color: white;
         }
       </style>
       ${walletsList()}
