654ac2c5f190dc8085b5938f74a39159b58c21f3
Spirobel <spirobel@protonmail.com>
2026-05-23 19:35:20 +0800

    use new integrated retry and connection status reading on manyscanchacheopened instance (result of openwallets() call)

--- background/background.ts
+++ background/background.ts
@@ -100,76 +100,24 @@ if (browser.runtime) {
     });
   });
 }
-let retryScheduled = false;
 let wallets = await initWallets();
 async function initWallets() {
   wallets?.stopWorker(); // kills all old worker threads
   if (!(await setupFinishedYet())) return;
   const local_wallets = await openWallets({
     notifyMasterChanged: (result) => {
-      doETA();
       sendWalletChangedEvent(result);
     },
     workerError: async (err) => {
       if (!(await setupFinishedYet())) return;
-
       console.log(
         "scan worker error, typically loss of network connection, retry in 1 second",
         err,
       );
-      if (retryScheduled) return;
-
-      retryScheduled = true;
-      setTimeout(() => {
-        local_wallets?.retry();
-        retryScheduled = false;
-      }, 1000);
     },
+    autoRetry: true,
+    retryDelayMs: 1000,
     no_stats: true,
   });
   return local_wallets;
-}
-let blocks_till_tip = null;
-let last_height: null | number = null;
-let last_height_update_timestamp: null | number = null;
-let blocks_since_last_update: null | number = null;
-let duration: null | number = null;
-function doETA() {
-  if (!wallets?.wallets) return;
-  const wallet = wallets.wallets[0];
-  const daemon_height = wallet?.daemon_height;
-  const current_height = wallet?.current_height;
-  if (typeof daemon_height !== "number" || typeof current_height !== "number")
-    return;
-
-  blocks_till_tip = daemon_height - current_height;
-  if (typeof last_height === "number")
-    blocks_since_last_update = current_height - last_height;
-
-  if (typeof last_height_update_timestamp === "number") {
-    duration = Date.now() - last_height_update_timestamp;
-  }
-
-  last_height = current_height;
-  last_height_update_timestamp = Date.now();
-
-  if (blocks_since_last_update !== null && duration !== null) {
-    const blocks_per_ms = blocks_since_last_update / duration;
-    const eta = blocks_till_tip / blocks_per_ms;
-    function msToHHMM(ms: number): string {
-      const totalSeconds = Math.floor(ms / 1000);
-      const minutes = Math.floor(totalSeconds / 60);
-      const hours = Math.floor(minutes / 60);
-      const remainingMinutes = minutes % 60;
-      if (!Number.isFinite(hours) || !Number.isFinite(minutes)) {
-        return "00:00";
-      }
-
-      const paddedHours = String(hours).padStart(2, "0");
-      const paddedMinutes = String(remainingMinutes).padStart(2, "0");
-
-      return `${paddedHours}:${paddedMinutes}`;
-    }
-    atomicWrite("eta.json", JSON.stringify({ eta: msToHHMM(eta) }, null, 2));
-  }
 }
--- wallet/sidebar/segments/walletRoute.ts
+++ wallet/sidebar/segments/walletRoute.ts
@@ -1,8 +1,4 @@
-import {
-  get_info,
-  readConnectionStatusDefaultLocation,
-  type ManyScanCachesOpened,
-} from "@spirobel/monero-wallet-api";
+import { type ManyScanCachesOpened } from "@spirobel/monero-wallet-api";
 import type { Mini } from "../../../mininext/mininext";
 import { router, type WalletRouteParams } from "../router";
 import { attachHandlers } from "../ui/buttons";
@@ -81,77 +77,14 @@ export async function setCurrentStartingHeight(start_height: number | null) {
   if (!window.wallets?.wallets) throw new Error("no wallets");
   await window.wallets.changeStartHeight(start_height);
 }
-let interval: null | number | NodeJS.Timeout = null;
-let connected_to_node = false;
-let etaString: string | null = null;
-
 export function eta(): string | null {
-  return etaString;
+  return (
+    window.wallets?.connectionStatusOpened?.connectionStatus?.sync?.eta ?? null
+  );
 }
 
 export function connectedToNode(): boolean {
-  if (!interval) interval = setInterval(checkConnection, 2500);
-  return connected_to_node;
-}
-
-async function checkConnection() {
-  const jsonString = await Bun.file("eta.json")
-    .text()
-    .catch(() => null);
-  if (jsonString) {
-    const etaJSON = JSON.parse(jsonString);
-    if (etaJSON.eta) {
-      etaString = etaJSON.eta;
-    }
-  }
-  const connectionStatus = await readConnectionStatusDefaultLocation();
-  if (
-    (connectionStatus?.last_packet.status === "OK" ||
-      connectionStatus?.last_packet.status === "blocks_buffer_full") &&
-    isWithinLast10Seconds(connectionStatus?.last_packet.timestamp)
-  ) {
-    connected_to_node = true;
-  } else if (
-    connectionStatus?.last_packet.status !== "OK" &&
-    connectionStatus?.last_packet.status !== "blocks_buffer_full"
-  ) {
-    connected_to_node = false; // if connection status is not OK, we assume no connection
-    activeConnectionCheck(); // still tx processing, takes time, so we do an active check
-  } else {
-    connected_to_node = true;
-    activeConnectionCheck();
-  }
-}
-async function activeConnectionCheck() {
-  if (!window.wallets?.wallets) {
-    connected_to_node = false;
-    return;
-  }
-  const wallet = window.wallets.wallets[0];
-  const node_url = wallet?.node_url;
-  if (!node_url) {
-    connected_to_node = false;
-    return;
-  }
-  try {
-    await get_info(wallet?.node_url);
-    connected_to_node = true;
-    return;
-  } catch (error) {
-    connected_to_node = false;
-    return;
-  }
-}
-function isWithinLast10Seconds(timestamp?: string): boolean {
-  if (!timestamp) return false;
-  const date = new Date(timestamp);
-  if (isNaN(date.getTime())) {
-    return false;
-  }
-  const now = Date.now();
-  const tsTime = date.getTime();
-  const ageInMs = now - tsTime;
-  return ageInMs >= 0 && ageInMs <= 10_000;
+  return window.wallets?.connectionStatusOpened?.isConnected ?? false;
 }
 
 export function disnavigate() {
